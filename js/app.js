4af47fbd075a1edf5ce7476a039b6e3df4840898
Eddie <eddie@annix.com.au>
2024-04-09 21:41:41 +0800

    init

---
+++ js/app.js
@@ -0,0 +1,64 @@
+var app = new Vue({
+	el: '#app',
+	data: {
+		scanner: null,
+		activeCameraId: null,
+		cameras: [],
+		attendances: []
+	},
+	mounted: function () {
+		var self = this;
+		self.scanner = new Instascan.Scanner({ video: document.getElementById('preview'), scanPeriod: 5 });
+		self.scanner.addListener('scan', function (content, image) {
+			self.attendances.unshift({ date: +(Date.now()), content: content });
+		});
+		Instascan.Camera.getCameras().then(function (cameras) {
+			self.cameras = cameras;
+			if (cameras.length > 0) {
+				self.activeCameraId = cameras[0].id;
+				self.scanner.start(cameras[0]);
+			} else {
+				console.error('No cameras found.');
+			}
+		}).catch(function (e) {
+			console.error(e, self.attendances);
+		});
+	},
+	methods: {
+		formatName: function (name) {
+			return name || '(unknown)';
+		},
+		selectCamera: function (camera) {
+			this.activeCameraId = camera.id;
+			this.scanner.start(camera);
+		}
+	}
+});
+
+function generateQRCode(e) {
+	e.preventDefault();
+	var id = document.getElementById('id').value;
+	var firstname = document.getElementById('firstname').value;
+	var lastname = document.getElementById('lastname').value;
+
+	var qrInput = JSON.stringify({ id: id, firstname: firstname, lastname: lastname });
+	console.log(qrInput);
+	var qrCodeContainer = document.getElementById("qr-code");
+
+	QRCode.toCanvas(qrCodeContainer, qrInput, function (error) {
+		if (error) console.error(error)
+		console.log('success!');
+		/* var image = new Image();
+		image.src = qrCodeContainer.toDataURL("image/png");
+		var link = document.createElement('a');
+		link.download = id+'-qrcode.png';
+		link.href = image.src;
+		link.click(); */
+		
+		var pngData = qrCodeContainer.toDataURL("image/png");
+		var img = document.getElementById("qr-code-img");
+		img.src = pngData;
+		qrCodeContainer.style.display = 'none';
+		img.style.display = 'block';
+	});
+}
