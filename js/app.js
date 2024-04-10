aae52f9e74bb2cb44662e654e5ac16f094f4e7f0
Eddie <eddie@annix.com.au>
2024-04-10 11:47:19 +0800

    updates

--- js/app.js
+++ js/app.js
@@ -15,7 +15,8 @@ var app = new Vue({
 				var json = JSON.parse(content);
 				json['date'] = getDateNow();
 				if (saveLocal(json)) {
-					self.scans.unshift({ data: json, date: +(Date.now()), content: (json.lastname + ', ' + json.firstname) });
+					// self.scans.unshift({ data: json, date: +(Date.now()), content: (json.lastname + ', ' + json.firstname) });
+					appendData(self, json);
 					alert('"' + json.lastname + ', ' + json.firstname +'" was Successfully logged in!');
 				} else {
 					alert('"' + json.lastname + ', ' + json.firstname +'" Already Exist!');
@@ -96,17 +97,79 @@ function generateQRCode(e) {
 
 	var qrInput = JSON.stringify(json);
 	// console.log(qrInput);
-	var qrCodeContainer = document.getElementById("qr-code");
+	var qrCodeCanvas = document.getElementById("qr-code");
 	var img = document.getElementById("qr-code-img");
 	img.style.display = 'none';
+	var shareBtn = document.getElementsByClassName('share-appended');
+	if (shareBtn.length) {
+		shareBtn[0].remove();
+	}
 
-	QRCode.toCanvas(qrCodeContainer, qrInput, function (error) {
+	QRCode.toCanvas(qrCodeCanvas, qrInput, function (error) {
 		if (error) console.error(error)
 		// console.log('success!');
-		var pngData = qrCodeContainer.toDataURL("image/png");
+		var pngData = qrCodeCanvas.toDataURL("image/png");
 		img.src = pngData;
-		qrCodeContainer.style.display = 'none';
+		qrCodeCanvas.style.display = 'none';
 		img.style.display = 'block';
+		
+		var shareBtn = document.createElement('img');
+		shareBtn.style = 'cursor: pointer; max-width: 20px; float: right; padding-right: 40px; margin-bottom: 10px;';
+		shareBtn.src = './share.png';
+		shareBtn.classList.add('share-appended');
+		shareBtn.addEventListener('click', async (e) => {
+			try {
+				// Check if the Web Share API is supported by the browser
+				if (navigator.share) {
+					var ctx = qrCodeCanvas.getContext('2d');
+					qrCodeCanvas.width = e.target.width;
+					qrCodeCanvas.height = e.target.height;
+					ctx.drawImage(e.target, 0, 0);
+					// Get the image data from the canvas
+					var imageData = ctx.getImageData(0, 0, qrCodeCanvas.width, qrCodeCanvas.height);
+					// Decode the QR code from the image data
+					var code = jsQR(imageData.data, imageData.width, imageData.height);
+
+					if (code) {
+						var json = JSON.parse(code.data);
+						// Fetch the image URL (replace 'image.jpg' with the actual path to your image)
+						var imageUrl = e.target.src;
+						// Fetch the image blob
+						var response = await fetch(imageUrl);
+						var blob = await response.blob();
+						var name = (json.lastname.trim() + '-' + json.firstname.trim());
+						// Create a file object from the image blob
+						var file = new File([blob], name + '_QRCode.jpg', { type: blob.type });
+						// Share the image using the Web Share API
+						await navigator.share({
+							files: [file],
+							title: 'Share QR Code',
+							text: name + '_QRCode.jpg'
+						});
+	
+						console.log('Image shared successfully.');
+					} else {
+						alert('Invalid QR Code!');
+					}
+				} else {
+					// Web Share API is not supported
+					console.error('Web Share API is not supported.');
+					alert('Web Share API is not supported in this browser.');
+				}
+			} catch (error) {
+				// Error occurred while sharing the image
+				console.error('Error sharing QR Code:', error);
+				alert('Error sharing QR Code. Please try again.');
+			}
+		});
+		img.parentNode.insertBefore(shareBtn, img.nextSibling);
+
+		json['date'] = getDateNow();
+		// console.log(json);
+		if (saveLocal(json)) {
+			appendData(app, json);
+			alert('"' + json.lastname + ', ' + json.firstname + '" was Successfully logged in!');
+		}
 	});
 }
 
@@ -159,27 +222,81 @@ function convertJSONToCSV(jsonData) {
 		return false;
 	}
 }
+
 // Get references to the drop zone and the preview image
 var dropZone = document.getElementById('dropZone');
 var previewImage = document.getElementById('qr-code-img');
 
-// Prevent default drag behaviors
-['dragenter', 'dragover', 'dragleave', 'drop'].forEach(eventName => {
-	dropZone.addEventListener(eventName, preventDefaults, false);
-});
-
-// Highlight drop area when dragging over it
-['dragenter', 'dragover'].forEach(eventName => {
-	dropZone.addEventListener(eventName, highlight, false);
-});
-
-// Unhighlight drop area when not dragging over it
-['dragleave', 'drop'].forEach(eventName => {
-	dropZone.addEventListener(eventName, unhighlight, false);
-});
-
-// Handle dropped files
-dropZone.addEventListener('drop', handleDrop, false);
+function runDragnDrop() {
+	// Prevent default drag behaviors
+	['dragenter', 'dragover', 'dragleave', 'drop'].forEach(eventName => {
+		dropZone.addEventListener(eventName, preventDefaults, false);
+	});
+	
+	// Highlight drop area when dragging over it
+	['dragenter', 'dragover'].forEach(eventName => {
+		dropZone.addEventListener(eventName, highlight, false);
+	});
+	
+	// Unhighlight drop area when not dragging over it
+	['dragleave', 'drop'].forEach(eventName => {
+		dropZone.addEventListener(eventName, unhighlight, false);
+	});
+	
+	// Handle dropped files
+	dropZone.addEventListener('drop', function (event) {
+		var files = event.dataTransfer.files;
+		if (files.length > 0) {
+			var file = files[0];
+			if (file.type.startsWith('image/')) {
+				var reader = new FileReader();
+				reader.onload = function (event) {
+					previewImage.src = event.target.result;
+					previewImage.style.display = 'block';
+	
+					var qrCodeCanvas = document.getElementById("qr-code");
+	
+					setTimeout(() => {
+						// Create a canvas element to draw the image
+						var ctx = qrCodeCanvas.getContext('2d');
+						qrCodeCanvas.width = previewImage.width;
+						qrCodeCanvas.height = previewImage.height;
+						ctx.drawImage(previewImage, 0, 0);
+						// Get the image data from the canvas
+						var imageData = ctx.getImageData(0, 0, qrCodeCanvas.width, qrCodeCanvas.height);
+						// Decode the QR code from the image data
+						var code = jsQR(imageData.data, imageData.width, imageData.height);
+	
+						if (code) {
+							var json = JSON.parse(code.data);
+							json['date'] = getDateNow();
+							// console.log(json);
+							if (saveLocal(json)) {
+								appendData(app, json);
+								alert('"' + json.lastname + ', ' + json.firstname + '" was Successfully logged in!');
+							} else {
+								alert('"' + json.lastname + ', ' + json.firstname + '" Already Exist!');
+							}
+						} else {
+							alert('Invalid QR Code!');
+						}
+	
+						previewImage.style.display = 'none';
+						var shareBtn = document.getElementsByClassName('share-appended');
+						if (shareBtn.length) {
+							shareBtn[0].remove();
+						}
+					}, 1000);
+	
+					qrCodeCanvas.style.display = 'none';
+				};
+				reader.readAsDataURL(file);
+			} else {
+				alert('Please drop an image file.');
+			}
+		}
+	}, false);
+}
 
 function preventDefaults(event) {
 	event.preventDefault();
@@ -194,54 +311,7 @@ function unhighlight() {
 	dropZone.classList.remove('highlight');
 }
 
-function handleDrop(event) {
-	var files = event.dataTransfer.files;
-	if (files.length > 0) {
-		var file = files[0];
-		if (file.type.startsWith('image/')) {
-			var reader = new FileReader();
-			reader.onload = function (event) {
-				previewImage.src = event.target.result;
-				previewImage.style.display = 'block';
-
-				var qrCodeContainer = document.getElementById("qr-code");
-
-				setTimeout(() => {
-					// Create a canvas element to draw the image
-					var ctx = qrCodeContainer.getContext('2d');
-					qrCodeContainer.width = previewImage.width;
-					qrCodeContainer.height = previewImage.height;
-					ctx.drawImage(previewImage, 0, 0);
-					// Get the image data from the canvas
-					var imageData = ctx.getImageData(0, 0, qrCodeContainer.width, qrCodeContainer.height);
-					// Decode the QR code from the image data
-					var code = jsQR(imageData.data, imageData.width, imageData.height);
-
-					if (code) {
-						var json = JSON.parse(code.data);
-						json['date'] = getDateNow();
-						// console.log(json);
-						if (saveLocal(json)) {
-							app.scans.unshift({ data: json, date: +(Date.now()), content: (json.lastname + ', ' + json.firstname) });
-							alert('"' + json.lastname + ', ' + json.firstname +'" was Successfully logged in!');
-						} else {
-							alert('"' + json.lastname + ', ' + json.firstname +'" Already Exist!');
-						}
-					} else {
-						alert('Invalid QR Code!');
-					}
-
-					previewImage.style.display = 'none';
-				}, 1000);
-				
-				qrCodeContainer.style.display = 'none';
-			};
-			reader.readAsDataURL(file);
-		} else {
-			alert('Please drop an image file.');
-		}
-	}
-}
+runDragnDrop();
 
 function getDateNow(timestamp) {
 	// Create a new Date object
@@ -264,4 +334,30 @@ function getDateNow(timestamp) {
 
 	// console.log(dateTimeString); // Output the formatted date and time
 	return dateTimeString;
+}
+
+function appendData(app, json) {
+	var qrCodeCanvas = document.createElement('canvas');
+	QRCode.toCanvas(qrCodeCanvas, JSON.stringify(json), function (error) {
+		if (error) console.error(error)
+		var pngData = qrCodeCanvas.toDataURL("image/png");
+		var title = (json.lastname + ', ' + json.firstname);
+		json['img_url'] = pngData;
+		app.scans.unshift({ data: json, date: +(Date.now()), title: title, content: '<b>' + title + '</b><img src="' + pngData + '" style="max-width: 28px; float: right;" />' });
+	});
+}
+
+async function shareQR(params) {
+	// console.log(params.data);
+	if (navigator.share) {
+		await navigator.share({
+			files: [params.data.img_url],
+			title: 'Share QR Code',
+			text: params.title + '_QRCode.jpg'
+		});
+	} else {
+		// Web Share API is not supported
+		console.error('Web Share API is not supported.');
+		alert('Web Share API is not supported in this browser.');
+	}
 }
